f674d8635428c89868cb2faa37f4f61a438a13bc
pd6974 <pdolan17@gmail.com>
2022-10-22 12:44:36 -0500

    Finishes ui functionality with more interactive buttons and restart option

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ let playerSelection;
 //Variables to track scores and round counts
 let computerScore = 0;
 let playerScore = 0;
-let roundCount = 0;
 
 //Variables to post the prompt and results from each round
 const div = document.querySelector('.results');
@@ -17,6 +16,7 @@ const prompt = document.querySelector('.prompt');
 const rockBtn = document.querySelector('.rock');
 const paperBtn = document.querySelector('.paper');
 const sciBtn = document.querySelector('.scissors');
+const restart = document.querySelector('.restart');
 
 //chose to make different event listeners because I couldn't figure out how to loop through all
 rockBtn.addEventListener('click', () => {
@@ -34,6 +34,14 @@ sciBtn.addEventListener('click', () => {
         winner(playRound(playerSelection));
     });
 
+restart.addEventListener('click', () => {
+    playerScore = 0;
+    computerScore = 0;
+    winner(playRound(playerSelection));
+    div.textContent = " ";
+    document.getElementById("restart").style.visibility = "hidden";
+})
+
 
 
 function getComputerChoice() {
@@ -50,7 +58,12 @@ function getComputerChoice() {
 
 
 function playRound(playerSelection) {
-    
+
+
+    if (playerScore >= 5 || computerScore >= 5) {
+        div.textContent = "Please press the restart button to play again."
+
+} else {
     computerSelection = getComputerChoice();
     console.log(playerSelection);
     playerSelection = playerSelection.toLowerCase();
@@ -63,26 +76,33 @@ function playRound(playerSelection) {
     }
     
     return winningMoveMap[playerSelection] === computerSelection ? 1 : 2;
+}
     
 }
 
 
 function winner(result) {
-    if(roundCount < 5) {
-        if (result === 0) {
-            div.textContent += "\nIt's a tie! Please choose again.\n";
-        } else if (result === 1) {
-            playerScore++;
-            div.textContent += "\nYou win! " + playerSelection +" beats " + computerSelection + "\n";
-            div.textContent += "\nPlayer Score: " + playerScore + "  Computer Score: " + computerScore + "\n";
-            roundCount++;
-        } else if (result === 2) {
-            computerScore++;
-            div.textContent += "\nYou lose! " + computerSelection + " beats " + playerSelection + "\n";
-            div.textContent += "\nPlayer Score: " + playerScore + "  Computer Score: " + computerScore + "\n";
-            roundCount++;
-        }
-    } else {
-
+    if (result === 0) {
+            div.textContent = "It's a tie! Please choose again.\nPlayer Score:" + playerScore + " Computer Score: " + computerScore;
+    } else if (result === 1) {
+        playerScore++;
+            if (playerScore <= 4) {
+                div.textContent = "You win! " + playerSelection +" beats " + computerSelection + "\nPlayer Score: "
+                 + playerScore + "  Computer Score: " + computerScore + "\n"; 
+            }
+            else {
+                div.textContent = "Congratulations! You beat the computer!  You shall reign as ruler of the office!";
+                document.getElementById("restart").style.visibility = "visible";
+            }           
+    } else if (result === 2) {
+        computerScore++;
+            if (computerScore <= 4) {
+                div.textContent = "You lose! " + computerSelection + " beats " + playerSelection + "\nPlayer Score: "
+             + playerScore + "  Computer Score: " + computerScore + "\n";            
+        }   else {
+            div.textContent = "Oh no!  You lost to the computer.  You must now submit to machines and stationary.";
+            document.getElementById("restart").style.visibility = "visible";
     }
 }
+}
+
